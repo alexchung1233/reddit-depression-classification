842f65ca842838a6f4235f0cf0b3d4140b2f77a7
monsterbasher1233 <39063219+monsterbasher1233@users.noreply.github.com>
2018-07-15 13:31:31 -0400

    Update data_collection_reddit.py

--- data_collection_reddit.py
+++ data_collection_reddit.py
@@ -1,6 +1,7 @@
 import praw
 import json
 import csv
+import pandas as pd
 #authenication
 reddit = praw.Reddit(
 client_id = 'QRPUsdVY1VjByw',
@@ -10,14 +11,34 @@ user_agent = 'classifier by /u/mmonsterbasher',
 username = 'mmonsterbasher'
 )
 
-depression = reddit.subreddit('depression')
-comments = []
-for submission in depression.hot(limit=20):
-    submission.comments.replace_more(limit=0)
-    comments.append(submission.comments.list())
-with open("depressed_comments.csv",'w') as csvfile:
-    writer = csv.writer(csvfile)
-    for comment in comments:
-        for c in comment:
-            writer.writerow([c.body])
-            print(c.body)
+def get_non_depressed_data_set(subreddits):
+    subreddits = list(map(reddit.subreddit, subreddits))
+    comments = []
+    for subreddit in subreddits:
+        for submission in subreddit.hot(limit=1000):
+            submission.comments.replace_more(limit=None)
+            for comment in submission.comments.list():
+                with open("non_depressed_comments.csv",'w',encoding='utf-8') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([comment.body])
+                print(comment.body)
+    
+                    
+      
+
+def get_depressed_data_set(subreddits):
+    subreddits = list(map(reddit.subreddit, subreddits))
+    comments = []
+    for subreddit in subreddits:
+        for submission in subreddit.hot(limit=400):
+            submission.comments.replace_more(limit=None)
+            comments.append(submission.comments.list())
+            
+    with open("depressed_comments.csv",'w',encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        for comment in comments:
+                for c in comment:
+                    writer.writerow([c.body])
+                    
+get_non_depressed_data(["news","lifeprotips","frugal","relationships","askreddit"])            
+
