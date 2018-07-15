823c9d9a49c5014dd04d83de2038ff64d151561b
monsterbasher1233 <39063219+monsterbasher1233@users.noreply.github.com>
2018-07-15 15:30:24 -0400

    Update data_collection_reddit.py

--- data_collection_reddit.py
+++ data_collection_reddit.py
@@ -17,7 +17,7 @@ def get_non_depressed_data_set(subreddits):
     for subreddit in subreddits:
         for submission in subreddit.hot(limit=1000):
             submission.comments.replace_more(limit=None)
-            for comment in submission.comments.list():
+            for comment in praw.helpers.flatten_tree(submission.comments):
                 with open("non_depressed_comments.csv",'w',encoding='utf-8') as csvfile:
                     writer = csv.writer(csvfile)
                     writer.writerow([comment.body])
@@ -30,15 +30,13 @@ def get_depressed_data_set(subreddits):
     subreddits = list(map(reddit.subreddit, subreddits))
     comments = []
     for subreddit in subreddits:
-        for submission in subreddit.hot(limit=400):
+        for submission in subreddit.hot(limit=1000):
             submission.comments.replace_more(limit=None)
-            comments.append(submission.comments.list())
-            
-    with open("depressed_comments.csv",'w',encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        for comment in comments:
-                for c in comment:
-                    writer.writerow([c.body])
+            for comment in praw.helpers.flatten_tree(submission.comments):
+                with open("non_depressed_comments.csv",'w',encoding='utf-8') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([comment.body])
+                print(comment.body)
                     
 get_non_depressed_data(["news","lifeprotips","frugal","relationships","askreddit"])            
-
+get_depressed_data(["depression","suicidewatch","mentalhealth","bipolar","depressed","anxiety"])  
